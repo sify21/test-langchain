3031526c1077fb6bf7e0f8fb5dc7fee81b83baa3
司芳源 <sify21@163.com>
2026-02-25 00:17:39 +0800

    d

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from test-langchain!")
+from langchain.agents import create_agent
+from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.messages import SystemMessage
+
+# model = ChatOpenAI()
+
+# gemini-3-pro-image-preview / gemini-3-pro-preview / gemini-3-flash-preview
+# 需要爬梯
+model = ChatGoogleGenerativeAI(model='gemini-3-pro-preview', vertexai=False)
+
+sysprompt = SystemMessage(content="you are a helpful assist agent")
+
+agent = create_agent(model, system_prompt=sysprompt, name="weatherman_assistant")
 
 
-if __name__ == "__main__":
-    main()
+result = agent.invoke({"messages": [{"role": "user", "content": "what is the weather in BeiJing"}]})
+
+print(result)
